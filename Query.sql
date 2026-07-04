9bd7c4a0c26f7d38b9b75f1d16d47507751abfe2
TheKhanSaheb <rakinulalam99@gmail.com>
2026-07-04 23:20:05 +0600

    crated user table
    cretare

--- Query.sql
+++ Query.sql
@@ -1,1 +1,8 @@
 
+CREATE TABLE Users (
+  user_id INT PRIMARY KEY,
+  full_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  role VARCHAR(50) CHECK (role IN ('Ticket Manager', 'Football Fan')),
+  phone_number VARCHAR(20)
+);
