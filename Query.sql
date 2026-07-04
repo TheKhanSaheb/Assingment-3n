76d65583c9bb69f33d2bf5aaeed4f48a7409afb2
TheKhanSaheb <rakinulalam99@gmail.com>
2026-07-04 23:20:44 +0600

    created matches table

--- Query.sql
+++ Query.sql
@@ -6,3 +6,19 @@ CREATE TABLE Users (
   role VARCHAR(50) CHECK (role IN ('Ticket Manager', 'Football Fan')),
   phone_number VARCHAR(20)
 );
+
+
+CREATE TABLE Matches (
+  match_id INT PRIMARY KEY,
+  fixture VARCHAR(100) NOT NULL,
+  tournament_category VARCHAR(50),
+  base_ticket_price DECIMAL(10, 2) CHECK (base_ticket_price >= 0),
+  match_status VARCHAR(20) CHECK (
+    match_status IN (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  )
+);
